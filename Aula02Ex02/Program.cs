16a2da96a9be24797fc05d0e066b49b41f2f1bd1
Mahãyana Vitória <84738174+MahayanaVitoria@users.noreply.github.com>
2022-08-18 15:29:11 -0300

    ex1-ex2

---
+++ Aula02Ex02/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try //tente executar isso
+            {
+                Console.WriteLine("Informe o valor R$:");
+                double real = Convert.ToDouble(Console.ReadLine()); //conversão de real string pra real double
+
+                double dolar = 5.17;
+                double euro = 6.14;
+                double peso = 0.05;
+                double dolarConversao = CalcularDolar(real, dolar);
+                double euroConversao = CalcularDolar(real, euro);
+                double pesoConversao = CalcularDolar(real, peso);
+
+                Console.WriteLine("R$" + real + " convertido em dólar é $" + dolarConversao);
+                Console.WriteLine("R$" + real + " convertido em euro é $" + euroConversao);
+                Console.WriteLine("R$" + real + " convertido em peso é $" + pesoConversao);
+
+            }
+            catch(Exception erro) //se não der, tente isso
+            {
+                Console.WriteLine(erro.Message);
+            }
+        }
+        
+        static double CalcularDolar(double real, double dolar) //função para calcular conversão de dólar
+        {
+            return real * dolar;
+
+        }
+        static double CalcularEuro(double real, double euro) //função para calcular conversão de dólar
+        {
+            return real * euro;
+
+        }
+        static double CalcularPeso(double real, double peso) //função para calcular conversão de dólar
+        {
+            return real * peso;
+
+        }
+    }
+}
